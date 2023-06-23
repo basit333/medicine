71ec773ed2a8268926c35b8322cb029685126ab8
basit333 <shahbasit741@gmail.com>
2023-06-24 03:05:02 +0500

    mobile design is completed

--- js/app.js
+++ js/app.js
@@ -3,68 +3,12 @@
 Swiper Slider
 ------------------------------
 */
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-//   loop: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   breakpoints: {
-//     640: {
-//       slidesPerView: 1,
-//       spaceBetween: 20,
-//     },
-//     768: {
-//       slidesPerView: 2,
-//       spaceBetween: 40,
-//     },
-//     1024: {
-//       slidesPerView: 3,
-//       spaceBetween: 50,
-//     },
-//   },
-// });
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  },
-});
-
-//
 
 var swiper = new Swiper(".mySwiper", {
   loop: true,
   scrollbar: {
     el: ".swiper-scrollbar",
-    hide: true,
+    // hide: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
@@ -76,7 +20,7 @@ var swiper = new Swiper(".mySwiper", {
       spaceBetween: 10,
     },
     768: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 20,
     },
     1024: {
